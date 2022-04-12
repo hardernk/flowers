90d899b2d2c12231d6669a02c7552f7227ac010b
Nina Harder <nkharder@gmail.com>
2022-04-12 21:03:55 +0200

    fix test

--- main_test.py
+++ main_test.py
@@ -2,9 +2,9 @@ import main
 
 
 def test_blooming_flowers():
-    assert main.blooming_flowers('may') == ['peonies', 'rhododendrons',
+    assert main.blooming_flowers('May') == ['peonies', 'rhododendrons',
                                             'lilac']
-    assert len(main.blooming_flowers('march')) == 3
+    assert len(main.blooming_flowers('March')) == 3
 
     assert main.blooming_flowers('mai') == []
     assert main.blooming_flowers('') == []
